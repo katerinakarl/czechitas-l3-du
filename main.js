48259172f0f075c30533d219cbf6e3f12f6614cc
Katerina Karlovska <karlovska.kat@gmail.com>
2023-02-06 15:13:58 +0100

    verze2

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 // tady je místo pro náš program
 
+let jmeno = document.querySelector ("#jmenoUzivatele");
+let vstupUzivatelJMENO = prompt("Zadejte Vaše jméno");
+
+jmenoUzivatele.innerHTML = vstupUzivatelJMENO;
+
+let prijmeni = document.querySelector ("#prijmeniUzivatele");
+let vstupUzivatelPrijmeni = prompt("Zadejte Vaše příjmení");
+
+prijmeniUzivatele.innerHTML = vstupUzivatelPrijmeni;
+
+let vek = document.querySelector ("#vekUzivatele");
+let vstupUzivatelVek = prompt("Zadejte Váš věk"); //NEFUNGUJE při zadání Number
+
+vekUzivatele.innerHTML = vstupUzivatelVek;
+
+let inicialyUzivatel = document.querySelector ("#InicialyUzivatele");
+
+if (vstupUzivatelVek != null) {
+    document.getElementById("InicialyUzivatele").innerText =
+    vstupUzivatelJMENO + " " +  vstupUzivatelPrijmeni + " " + vstupUzivatelVek;
+  }
+
+let barva = document.querySelector ("#oblibenabarva");
+let vstupUzivatelBarva = prompt("Zadejte anglický název pro vaši oblíbenou barvu ");
+
+oblibenabarva.innerHTML = vstupUzivatelBarva;
+
+document.getElementById("#mojeOblibenaBarva").style.color = vstupUzivatelBarva;
+
 
 
 
